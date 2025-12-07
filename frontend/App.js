012354e1e09ae10a7efb9c0a24d473af2de8dc97
R9jin <ticsrgt@gmail.com>
2025-12-07 21:40:58 +0800

    i

--- frontend/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function App() {
-  return (
-    <div>
-      <h1>Simple React App</h1>
-    </div>
-  );
-}
--- test.py
+++
@@ -1,9 +0,0 @@
-def update_hour():
-    tmp = hour
-    if is_dst:
-        tmp += 1
-    else:
-        tmp -= 1
-is_dst = True
-hour = int(input("Enter hour: "))
-update_hour()
